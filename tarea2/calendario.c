8fd072cfdbad87cdc38cb0b22e1022e46e543998
Julio Barra <julio.barravalenzuela@gmail.com>
2022-05-18 02:02:48 -0400

    Avance en parte 1 de la tarea

--- tarea2/calendario.c
+++ tarea2/calendario.c
@@ -5,7 +5,41 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main() {
-    printf("Hola mundo\n");
-    return 0;
+//declaramos el calendario globalmente
+static int scheduler[7][24];
+
+//creamos objeto de las tareas que hay que realizar
+struct tareas {
+    int horas, diaLimite, horaLimite;
+};
+
+int main() 
+{
+    int cantidadTareas;
+
+    int horas, diaLimite, horaLimite;
+
+    printf('Ingrese la cantidad de tareas que tiene para la semana : ');
+    scanf('%d', cantidadTareas);
+
+    struct tareas fechaTareas[cantidadTareas];
+
+    for(int i = 0; i < cantidadTareas; i++)
+    {
+        printf('Ingrese horas a dedicar : ');
+        scanf('%d', horas);
+        printf('Ingrese dia limite de la tarea : ');
+        scanf('%d', diaLimite);
+        printf('Ingrese horas limite de la tarea : ');
+        scanf('%d', horaLimite);
+
+        fechaTareas[i].horas = horas;
+        fechaTareas[i].diaLimite = diaLimite;
+        fechaTareas[i].horaLimite = horaLimite;
+    }
+
+    pthread_t pideDatos, ingresaDatos;
+    
+
+
 }
