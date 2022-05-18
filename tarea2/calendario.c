f404b8a447cd0554d9b508c36bba56ca95432764
Fitinpool <julio.barravalenzuela@gmail.com>
2022-05-18 16:51:20 -0400

    avance en tarea 2

--- tarea2/calendario.c
+++ tarea2/calendario.c
@@ -10,7 +10,7 @@ static int scheduler[7][24];
 
 //creamos objeto de las tareas que hay que realizar
 struct tareas {
-    int horas, diaLimite, horaLimite;
+    int horas, diaLimite, horaLimite, deadline;
 };
 
 int main() 
@@ -19,26 +19,35 @@ int main()
 
     int horas, diaLimite, horaLimite;
 
-    printf('Ingrese la cantidad de tareas que tiene para la semana : ');
-    scanf('%d', cantidadTareas);
+    printf("Ingrese la cantidad de tareas que tiene para la semana : ");
+    scanf("%d", &cantidadTareas);
 
     struct tareas fechaTareas[cantidadTareas];
 
     for(int i = 0; i < cantidadTareas; i++)
     {
-        printf('Ingrese horas a dedicar : ');
-        scanf('%d', horas);
-        printf('Ingrese dia limite de la tarea : ');
-        scanf('%d', diaLimite);
-        printf('Ingrese horas limite de la tarea : ');
-        scanf('%d', horaLimite);
+        printf("Tarea %d\n", i + 1);
+        printf("Ingrese horas a dedicar : ");
+        scanf("%d", &horas);
+        printf("Ingrese dia limite de la tarea : ");
+        scanf("%d", &diaLimite);
+        printf("Ingrese horas limite de la tarea : ");
+        scanf("%d", &horaLimite);
 
         fechaTareas[i].horas = horas;
         fechaTareas[i].diaLimite = diaLimite;
         fechaTareas[i].horaLimite = horaLimite;
+        fechaTareas[i].deadline = diaLimite * 24 + horaLimite;
+
     }
 
-    pthread_t pideDatos, ingresaDatos;
+    for(int i = 0; i < cantidadTareas; i++)
+    {
+        printf("Para tarea %d tenemos %d horas para el %d a las %d.\n", i + 1, fechaTareas[i].horas,fechaTareas[i].diaLimite, fechaTareas[i].horaLimite);
+    }
+
+
+    //pthread_t pideDatos, ingresaDatos;
     
 
 
