57497c1e25ddfdd97b823b47fcb522ed9ebef5c5
Julio Barra <julio.barravalenzuela@gmail.com>
2022-05-18 01:04:08 -0400

    primer commit para tareas

---
+++ tarea2/calendario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+    printf("Hola mundo\n");
+    return 0;
+}
